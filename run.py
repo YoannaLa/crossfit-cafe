5be98b69c36ec15407a40ace4722a370887e8bdd
YoannaLa <joannaelazarek@yahoo.co.uk>
2021-10-07 13:51:01 +0000

    reinstalling gspread and auth acounts

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ SCOPE = [
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scope(SCOPE)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('cross_fit_cafe_shakes')
 
