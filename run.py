4b5ed7f032517e1721bd58c679efbb600ad4353f
YoannaLa <joannaelazarek@yahoo.co.uk>
2021-10-07 15:34:57 +0000

    stock numbers connected and last row function added

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -54,4 +55,38 @@ def validate_data(values):
     return True
 
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with list data provided
+    """
+    print('Sales Working updating...\n')
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('Sales worksheet updated!.\n')
+
+
+def calculating_surplus_data(sales_row):
+    """
+    Compare sales with stock and work out the surplus for each shake.
+    The surplus is a differents between sales and stock:
+     - Positive number shows waste
+     - Negative number shows shakes made as customer waits as stock sold out.
+    """
+    print("Working out the surplus numbers.\n")
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculating_surplus_data(sales_data)
+
+
+print('Welcome to CrossFit Cafe data collection.')
+main()
