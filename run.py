f2b8865ed53c47e8ebbdd3dd3241bc02c86f3a00
YoannaLa <joannaelazarek@yahoo.co.uk>
2021-10-18 14:47:17 +0000

    fixing errors coming out on PEP8

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def get_sales_data():
         print("Example: 5,11,8,6,9,\n")
 
         data_str = input('Enter your sales numbers here:\n')
-       
+
         sales_data = data_str.split(",")
         validate_data(sales_data)
 
@@ -84,23 +84,6 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def get_last_4_weeks_sales():
-    """
-    Collect sales of each shake from last 4 weeks,
-    data per collumn, and return the data.
-    """
-    sales = SHEET.worksheet('Sales')
-
-    detox_column_last_4_weeks = str(sales.col_values(1)[-4:])
-    cleanser_column_last_4_weeks = str(sales.col_values(2)[-4:])
-    protein_column_last_4_weeks = str(sales.col_values(3)[-4:])
-    cherry_twist_column_last_4_weeks = str(sales.col_values(4)[-4:])
-    green_column_last_4_weeks = str(sales.col_values(5)[-4:])
-    results = 'Last 4 weeks Sales: \n Detox %s \n Cleanse %s \n Protein %s \n Cherry twist %s \n Green twist %s' % (detox_column_last_4_weeks, cleanser_column_last_4_weeks, protein_column_last_4_weeks, cherry_twist_column_last_4_weeks, green_column_last_4_weeks)
-    
-    return results
-    
-   
 def get_max_value_from_list(sale_value_list):
     max = 0
     for sale_value in sale_value_list:
@@ -109,26 +92,29 @@ def get_max_value_from_list(sale_value_list):
             max = number_value
     return max
 
-  
+
 def most_selling_product_last_4_weeks():
     """
     Most popular shakes in last 4 weeks
     """
     sales = SHEET.worksheet('Sales')
+    most_selling_product_last_4_weeks = []
+    
     detox_column_last_4_weeks = sales.col_values(1)[-4:]
     cleanser_column_last_4_weeks = sales.col_values(2)[-4:]
     protein_column_last_4_weeks = sales.col_values(3)[-4:]
     cherry_twist_column_last_4_weeks = sales.col_values(4)[-4:]
     green_column_last_4_weeks = sales.col_values(5)[-4:]
+
     detox_max = get_max_value_from_list(detox_column_last_4_weeks)
     cleanser_max = get_max_value_from_list(cleanser_column_last_4_weeks)
     protein_max = get_max_value_from_list(protein_column_last_4_weeks)
     cherry_twist_max = get_max_value_from_list(cherry_twist_column_last_4_weeks)
     green_max = get_max_value_from_list(green_column_last_4_weeks)
+
     max_value_list = [detox_max, cleanser_max, protein_max, cherry_twist_max, green_max]
     max_value = get_max_value_from_list(max_value_list)
-    results = 'Max 4 weeks Sales: \n Detox %i \n Cleanse %i \n Protein %s \n Cherry twist %i \n Green twist %i' % (detox_max, cleanser_max, protein_max, cherry_twist_max, green_max)
- 
+
     if (max_value == detox_max):
         print('Detox is the most popular shake with: %i sold' % max_value)
     if (max_value == cleanser_max):
@@ -139,8 +125,9 @@ def most_selling_product_last_4_weeks():
         print('Cherry Twist is the most popular shake with: %i sold' % max_value)
     if (max_value == green_max):
         print('Green is the most popular shake this month with: %i sold' % max_value)
-  
+
     return most_selling_product_last_4_weeks
+
 
 def calculate_stock_data(data):
     """
@@ -149,13 +136,13 @@ def calculate_stock_data(data):
     stock = SHEET.worksheet('Stock')
     print('Calculating stock data...\n')
     new_stock_data = []
-    
+
     for column in data:
         int_column = [int(num) for num in column]
         average = sum(int_column) / len(int_column)
         stock_num = average
         new_stock_data.append(stock_num)
-    
+
     return new_stock_data
 
 
@@ -164,20 +151,20 @@ def get_stock_value(data):
     The next week shake numbers prediction
     """
     stock = SHEET.worksheet('Stock').get_all_values()
-    print('Numbers to prepare for next week:\n')
-    
+    print('\nNumbers to prepare for next week:\n')
+
     headings = stock[0]
     stock_row = stock[-1]
 
     stock_value = []
     for stock in zip(headings, stock_row):
         stock_value.append(stock)
-        
+
     for shakes_name, new_stock_value in zip(headings, stock_row):
         print(f'Shake: {shakes_name.upper()}, No: {new_stock_value}\n')
 
     return new_stock_value
-    
+
 
 def main():
     """
@@ -188,11 +175,12 @@ def main():
     update_worksheet(sales_data, "Sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "Surplus")
-    stock_data = calculate_stock_data(data)
+    stock_data = (data)
     update_worksheet(stock_data, "Stock")
     new_stock_value = calculate_stock_data(data)
     most_selling_product_last_4_weeks()
     get_stock_value(data)
+
 
 print('Welcome to CrossFit Cafe data collection.')
 main()
