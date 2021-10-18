83e370a6f2beda4a492ec13b50a5f910d7df3a94
YoannaLa <joannaelazarek@yahoo.co.uk>
2021-10-18 12:33:52 +0000

    adding main function to print

--- run.py
+++ run.py
@@ -23,8 +23,8 @@ def get_sales_data():
         print("Five numbers as each shakes types, separated by commas.")
         print("Example: 5,11,8,6,9,\n")
 
-        data_str = input("Enter your sales numbers here:\n")
-
+        data_str = input('Enter your sales numbers here:\n')
+       
         sales_data = data_str.split(",")
         validate_data(sales_data)
 
@@ -42,11 +42,10 @@ def validate_data(values):
     or less or more than 6 values
     """
     try:
-        [int(value) for value in values]
         if len(values) != 5:
             raise ValueError(
                 print(f'5 numbers required, you provided {len(values)}')
-        )
+                )
 
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
@@ -86,36 +85,77 @@ def calculate_surplus_data(sales_row):
 
 
 def get_last_4_weeks_sales():
-    """ 
+    """
     Collect sales of each shake from last 4 weeks,
-    data per collumn, and return the data as a list of lists.
+    data per collumn, and return the data.
     """
     sales = SHEET.worksheet('Sales')
-    #most_popular = 
 
-    columns = []
-    for ind in range(1, 6):
-        column = sales.col_values(ind)
-        #most_popular =
-        columns.append(column[-4:])
-
-    return columns
+    detox_column_last_4_weeks = str(sales.col_values(1)[-4:])
+    cleanser_column_last_4_weeks = str(sales.col_values(2)[-4:])
+    protein_column_last_4_weeks = str(sales.col_values(3)[-4:])
+    cherry_twist_column_last_4_weeks = str(sales.col_values(4)[-4:])
+    green_column_last_4_weeks = str(sales.col_values(5)[-4:])
+    results = 'Last 4 weeks Sales: \n Detox %s \n Cleanse %s \n Protein %s \n Cherry twist %s \n Green twist %s' % (detox_column_last_4_weeks, cleanser_column_last_4_weeks, protein_column_last_4_weeks, cherry_twist_column_last_4_weeks, green_column_last_4_weeks)
     
+    return results
+    
+   
+def get_max_value_from_list(sale_value_list):
+    max = 0
+    for sale_value in sale_value_list:
+        number_value = int(sale_value)
+        if number_value > max:
+            max = number_value
+    return max
+
+  
+def most_selling_product_last_4_weeks():
+    """
+    Most popular shakes in last 4 weeks
+    """
+    sales = SHEET.worksheet('Sales')
+    detox_column_last_4_weeks = sales.col_values(1)[-4:]
+    cleanser_column_last_4_weeks = sales.col_values(2)[-4:]
+    protein_column_last_4_weeks = sales.col_values(3)[-4:]
+    cherry_twist_column_last_4_weeks = sales.col_values(4)[-4:]
+    green_column_last_4_weeks = sales.col_values(5)[-4:]
+    detox_max = get_max_value_from_list(detox_column_last_4_weeks)
+    cleanser_max = get_max_value_from_list(cleanser_column_last_4_weeks)
+    protein_max = get_max_value_from_list(protein_column_last_4_weeks)
+    cherry_twist_max = get_max_value_from_list(cherry_twist_column_last_4_weeks)
+    green_max = get_max_value_from_list(green_column_last_4_weeks)
+    max_value_list = [detox_max, cleanser_max, protein_max, cherry_twist_max, green_max]
+    max_value = get_max_value_from_list(max_value_list)
+    results = 'Max 4 weeks Sales: \n Detox %i \n Cleanse %i \n Protein %s \n Cherry twist %i \n Green twist %i' % (detox_max, cleanser_max, protein_max, cherry_twist_max, green_max)
+ 
+    if (max_value == detox_max):
+        print('Detox is the most poluar shake with: %i sold' % max_value)
+    if (max_value == cleanser_max):
+        print('Cleanser is the most poluar shake with: %i sold' % max_value)
+    if (max_value == protein_max):
+        print('Protein is the most poluar shake with: %i sold' % max_value)
+    if (max_value == cherry_twist_max):
+        print('Cherry Twist is the most poluar shake with: %i sold' % max_value)
+    if (max_value == green_max):
+        print('Green is the most popluar shake this month with: %i sold' % max_value)
+        return results
+
 
 def calculate_stock_data(data):
     """
-    Average stock for each shake, adding 15%
+    Average stock for each shake
     """
     stock = SHEET.worksheet('Stock')
     print('Calculating stock data...\n')
     new_stock_data = []
-
+    
     for column in data:
         int_column = [int(num) for num in column]
         average = sum(int_column) / len(int_column)
-        stock_num = average * 1.5
-        new_stock_data.append(round(stock_num))
-
+        stock_num = average
+        new_stock_data.append(stock_num)
+    
     return new_stock_data
 
 
@@ -134,9 +174,9 @@ def get_stock_value(data):
         stock_value.append(stock)
         
     for shakes_name, new_stock_value in zip(headings, stock_row):
-        print(f'Shake:{shakes_name.upper()},number to prepare:{new_stock_value}\n')
+        print(f'Shake: {shakes_name.upper()}, No: {new_stock_value}\n')
 
-    return stock_value
+    return new_stock_value
     
 
 def main():
@@ -148,11 +188,11 @@ def main():
     update_worksheet(sales_data, "Sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "Surplus")
-    sales_columns = get_last_4_weeks_sales()
-    stock_data = calculate_stock_data(sales_columns)
+    stock_data = calculate_stock_data(data)
     update_worksheet(stock_data, "Stock")
+    new_stock_value = calculate_stock_data(data)
+    most_selling_product_last_4_weeks()
     get_stock_value(data)
-
 
 print('Welcome to CrossFit Cafe data collection.')
 main()
